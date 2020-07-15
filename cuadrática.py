1b08ddb4bb840aa2464cac7d67ecfa3cfb12bc55
Tostón <54345339+Dr0k1t@users.noreply.github.com>
2020-07-15 15:26:22 -0500

    Update cuadrática.py

--- cuadrática.py
+++ cuadrática.py
@@ -1,17 +1,16 @@
 import math
-
+#Simple implementación para resolver ecuaciones cuadráticas aún si sus resultados son complejos.
 def cuadratica(a, b, c):
-    #Resolver ecuaciones cuadráticas
-    print("La ecuación que quieres resolver es", int(a), "X^2 +", int(b), "X +", int(c), " = 0")
-    det = b ** 2 -4*a*c
-    if det >= 0:
+    print("La ecuación que quieres resolver es", a, "x^2 +", b, "x +", c, " = 0")
+    det = b ** 2 -4*a*c #Calcula el determiante para simplificar escritura
+    if det >= 0: #Si los resultados son positivos
         x1= (math.sqrt(det) - b) / (2*a)
         x2= (math.sqrt(det) + b) / (2*a)
         print("Los resultados son: ", "\nx_1 = ", x1,"\nx_2 = ", x2) 
-    else:
+    else: #Si los resultados son complejos
         print("La ecuación no tiene soluciones reales.")
         print("Las soluciones complejas son: ")
-        print("x_1 = ", -1*b/(2*a),"+",(-det),"/",(2*a),"i\n","x_2 = ", -b/(2*a),"-",(-det),"/",(2*a),"i")
+        print("\nx_1 = ", -b, "/",(2*a),"+",(-det),"i/",(2*a),"\nx_2 = ",-b,"/",(2*a),"-",(-det),"i/",(2*a))
 
 x = input("Ingrese los valores de los coeficientes de la ecuación:\n a = ")
 y = input("b = ")
